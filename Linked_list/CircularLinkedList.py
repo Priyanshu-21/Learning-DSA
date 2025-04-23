d314d028bb13ff5359ee08b744a0a6592e65b761
Priyanshu-21 <priyanshu21031@gmail.com>
2025-04-23 22:08:56 +0530

    CDLL Insert(Begin)

--- Linked_list/CircularLinkedList.py
+++ Linked_list/CircularLinkedList.py
@@ -1,47 +1,49 @@
 # Implementation of Circular Linked List 
 '''
 1. Insert at Begining: - 
-
+    TC: O (N)
+    SC: O (N)
 '''
-class Node: 
+class Node:
     def __init__(self, data):
-        self.prev = None
-        self.data = data
+        self.prev = None 
+        self.data = data 
         self.next = None 
 
-class CircularLinkedList: 
+class DoublyLinkedList:
     def __init__(self):
         self.head = None 
-
+    
     def insertAtBegin(self, data):
         newNode = Node(data)
         if (self.head == None):
             self.head = newNode
-        else: 
-            # Wrong Implementation of circular Linked List need to check on this 
-            current = self.head
-            curHead = self.head
-            while (current == None):
-                current = current.next
-                if (current == self.head):
-                    exit
-            
+            newNode.prev = newNode.next = self.head 
+        else:
+            lastNode = self.head.prev
+            # update the previous & next point refrences 
+            newNode.next = self.head 
+            newNode.prev = lastNode
+            # update the last node previous & next point refrences 
+            self.head.prev = newNode
+            lastNode.next = newNode  # key point to make DLL circular 
+
             self.head = newNode
-            newNode.next = curHead
-            curHead.prev = newNode
-            current.next = self.head 
+
 
         return self.head
     
     def traverse(self):
-        slow = self.head
-        while (slow):
-            print(str(slow.data) + " --> ", end=" ")
-            slow = slow.next
+        current = self.head 
+        while (current):
+            print(str(current.data) + " --> ", end=" ")
+            current = current.next
+            
+
 
 if __name__ == "__main__":
-    cll = CircularLinkedList()
+    cll = DoublyLinkedList()
     cll.insertAtBegin(10)
-    cll.insertAtBegin(5)
-    cll.insertAtBegin(15)
+    cll.insertAtBegin(20)
+    cll.insertAtBegin(30)
     cll.traverse()
