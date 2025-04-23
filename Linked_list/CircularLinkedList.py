bec9fcab68dad80a55354d620ebee5ae92361f3b
Priyanshu-21 <priyanshu21031@gmail.com>
2025-04-23 22:50:05 +0530

    CDLL Insert(End)

--- Linked_list/CircularLinkedList.py
+++ Linked_list/CircularLinkedList.py
@@ -1,8 +1,11 @@
 # Implementation of Circular Linked List 
 '''
 1. Insert at Begining: - 
+    TC: O (1)
+    SC: O (1)
+2. Insert at End: - 
     TC: O (N)
-    SC: O (N)
+    TC: O (N)
 '''
 class Node:
     def __init__(self, data):
@@ -33,12 +36,35 @@ class DoublyLinkedList:
 
         return self.head
     
+    def insertAtEnd(self, data):
+        newNode = Node(data)
+        if (self.head == None):
+            self.head = newNode
+            newNode.prev = newNode.next = newNode
+        else:
+            # Traverse the linked Node and find the last Node 
+            lastNode = self.head
+            while (lastNode and not(lastNode.next == self.head)):
+                lastNode = lastNode.next
+            
+            # Update newNode references 
+            newNode.prev = lastNode
+            newNode.next = self.head 
+            
+            # Update lastNode and related references 
+            lastNode.next = newNode
+            self.head.prev = newNode
+        
+        return self.head
+                
+    
     def traverse(self):
         current = self.head 
         while (current):
             print(str(current.data) + " --> ", end=" ")
             current = current.next
-            
+            if (current == self.head):
+                break
 
 
 if __name__ == "__main__":
@@ -46,4 +72,8 @@ if __name__ == "__main__":
     cll.insertAtBegin(10)
     cll.insertAtBegin(20)
     cll.insertAtBegin(30)
+    cll.insertAtEnd(40)
+    cll.insertAtEnd(50)
     cll.traverse()
+
+    # Output: - 30 -->  20 -->  10 -->  40 -->  50 (Correct Expectation)
