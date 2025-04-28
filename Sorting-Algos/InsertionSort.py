ff57deebce543c9413157b839e1accc3fb11c246
Priyanshu-21 <priyanshu21031@gmail.com>
2025-04-28 10:19:42 +0530

    Sorting: Insertion

--- Sorting-Algos/InsertionSort.py
+++ Sorting-Algos/InsertionSort.py
@@ -1,18 +1,34 @@
 # Implementation of Insertion Sorting Algorithm 
+import ast, time, random
+
 def insertionSort(array, n):
     for i in range(1, n):
         key = array[i]
         j = i - 1
-        while (j > 0 and array[j] > key):
+        while (j >= 0 and array[j] > key):
             array[j + 1] = array[j]
+            array[j] = key
             j = j - 1
-        array[j + 1] = key
     return array
 
+array = []
+for i in range(1, 100000):
+    array.append(random.randint(1, 100))
 
-array = [2, 4, 7, 15, 5, 8, 3, 1]
+with open("./Sorting-Algos/input.txt", "w") as file:
+    file.write(str(array))
+
+with open("./Sorting-Algos/input.txt", "r") as file: 
+    array = ast.literal_eval(file.readline())
+    
 n = len(array)
+startTime = time.perf_counter()
 result = insertionSort(array, n)
-print("Sorted array: \n", result)
+endTime = time.perf_counter()
 
-# Wrong implementation of Insertion Sort algorithm (Need to check on this)
+# Save Sorted Array in output file
+with open("./Sorting-Algos/InsertionSort_output.txt", "w") as file:
+    file.write(str(result))
+
+#print("Sorted array: \n", result)
+print("Array Sorted in: ", endTime - startTime)
